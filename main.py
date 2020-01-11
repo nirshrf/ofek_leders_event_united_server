99d5a55fec360cb7ab6ca255e84d3511ee4bbe78
nir sherf <nir199820@gmail.com>
2020-01-11 18:03:02 +0200

    removed old and not used code

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ class ServerHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers['Content-Length'])
         post_body = self.rfile.read(content_len)
-        coordinates = self.parse_to_coordinates(post_body)
-        print(coordinates)
+
 #       Check the graphql query
         service_url = "http://localhost:9000/graphql"
         request_handler = GraphQLRequests(service_url)
@@ -24,14 +23,6 @@ class ServerHandler(SimpleHTTPRequestHandler):
         print(request_handler.import_adoptees(1))
         print(request_handler.import_AI_status())
 
-    def convert_post_message_to_dictionary(self, data):
-        return json.loads(data)
-
-    def parse_to_coordinates(self, data):
-        coordinates_dictionary = self.convert_post_message_to_dictionary(data)
-        if self.validate_more_than_one_animal_passed(coordinates_dictionary):
-            return [Coordinate(*pair) for pair in coordinates_dictionary['coordinates']]
-        return Coordinate(*coordinates_dictionary['coordinates'])
 
     def validate_more_than_one_animal_passed(self, coordinates_dictionary):
         return isinstance(coordinates_dictionary['coordinates'][0], list)
